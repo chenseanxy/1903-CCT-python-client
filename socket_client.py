20b6b55c7d3365b0045dfbad616f44ce26d58c28
chenxy <git@chenxy.tech>
2019-05-14 22:29:05 +0800

    Multithreading

--- socket_client.py
+++ socket_client.py
@@ -3,6 +3,7 @@ import os
 import time
 from tqdm import tqdm
 import sys
+import threading
 
 json = os.path.join(".", "record.json")
 
@@ -17,10 +18,27 @@ with open(json, mode="r", encoding="utf-8") as f:
     for line in tqdm(f):
         lines.append(line)
 
-for line in tqdm(lines):
-    # print(line)
-    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    s.connect((sys.argv[1], 5600))
-    s.send(bytes(line, "utf-8"))
-    s.close()
-    # time.sleep(0.01)
+
+def run(lines, numThreads, threadID):
+    for i in tqdm(range(int(len(lines)/numThreads))):
+        line = lines[i*10 + threadID]
+        # print(line)
+        s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        s.connect((sys.argv[1], 5600))
+        s.send(bytes(line, "utf-8"))
+        s.close()
+        # time.sleep(0.01)
+
+
+numThreads = 10
+threads = []
+
+for threadID in range(numThreads):
+    t = threading.Thread(target = run, args=(lines, numThreads, threadID))
+    threads.append(t)
+    t.start()
+
+for thread in threads:
+    thread.join()
+
+
