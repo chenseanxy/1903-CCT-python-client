634ac21248ac38963c0514260809880a1fa231c2
chenxy <git@chenxy.tech>
2019-05-14 22:17:50 +0800

    Load first

--- socket_client.py
+++ socket_client.py
@@ -10,11 +10,17 @@ if len(sys.argv) < 2:
     print("Please specify ip")
     exit()
 
+lines = []
+
 with open(json, mode="r", encoding="utf-8") as f:
+    print("Loading...")
     for line in tqdm(f):
-        # print(line)
-        s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        s.connect((sys.argv[1], 5600))
-        s.send(bytes(line, "utf-8"))
-        s.close()
-        # time.sleep(0.01)
+        lines.append(line)
+
+for line in tqdm(lines):
+    # print(line)
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    s.connect((sys.argv[1], 5600))
+    s.send(bytes(line, "utf-8"))
+    s.close()
+    # time.sleep(0.01)
